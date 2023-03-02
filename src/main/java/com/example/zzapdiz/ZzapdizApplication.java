49354bd89103a5bab183bdc28ace6a77bfc37fc7
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2023-03-03 01:46:00 +0900

    Initial commit

---
+++ src/main/java/com/example/zzapdiz/ZzapdizApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzapdiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzapdizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzapdizApplication.class, args);
+    }
+
+}

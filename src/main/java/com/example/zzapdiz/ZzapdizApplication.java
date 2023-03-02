b30eb07e6ad67414356441090b898a686fd58ff4
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2023-03-03 02:10:56 +0900

    어플리케이션 실행 확인

--- src/main/java/com/example/zzapdiz/ZzapdizApplication.java
+++ src/main/java/com/example/zzapdiz/ZzapdizApplication.java
@@ -8,6 +8,7 @@ public class ZzapdizApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZzapdizApplication.class, args);
+        System.out.println("어플리케이션 실행 ~~~~");
     }
 
 }

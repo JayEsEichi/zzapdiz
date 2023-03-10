e54f5bc2bde16af1e8ad54e663fd028de40a6079
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2023-03-10 17:31:50 +0900

    회원가입 api 주석 처리 방식 수정

--- src/main/java/com/example/zzapdiz/member/controller/MemberController.java
+++ src/main/java/com/example/zzapdiz/member/controller/MemberController.java
@@ -6,11 +6,7 @@ import com.example.zzapdiz.share.ResponseBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/zzapdiz")
@@ -19,8 +15,8 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입
-    @PostMapping("/signup")
+    /** 회원가입 **/
+    @PostMapping("/member/signup")
     public ResponseEntity<ResponseBody> memberSignUp(@RequestBody MemberSignupRequestDto memberSignupRequestDto){
         log.info("회원가입 api : 이메일 - {}, 이름 - {}", memberSignupRequestDto.getEmail(), memberSignupRequestDto.getMemberName());
 

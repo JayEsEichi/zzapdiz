69281111ae713c5fd282889641572daf5192737b
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2023-06-19 16:48:16 +0900

    Docker-Compose 파일, Dockerfile 파일 생성 및 Docker 구축 테스트를 위한 임의 api 생성

--- src/main/java/com/example/zzapdiz/ZzapdizApplication.java
+++ src/main/java/com/example/zzapdiz/ZzapdizApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @EnableRedisHttpSession
@@ -16,6 +19,12 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableJpaAuditing
 @SpringBootApplication
 public class ZzapdizApplication {
+
+    // Docker 구축 테스트
+    @RequestMapping("/")
+    public ResponseEntity<String> buildResponse(){
+        return new ResponseEntity<>("안녕 도커로 켜진 스프링아", HttpStatus.OK);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZzapdizApplication.class, args);
